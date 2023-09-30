5ea1341fe359dca3428a3ad577e1eef646fef705
Andres Arevalo Fajardo <a.arevalof@uniandes.edu.co>
2023-09-30 07:32:05 -0500

    update health

--- rasi_medical/rasi_medical/urls.py
+++ rasi_medical/rasi_medical/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('sede/', include('sede.urls')),
     path('equipo/', include('equipo_medico.urls')),
-    path('health-check', views.health_check, name='health_check')
+    path('health-check/', views.health_check, name='health_check')
 ]
