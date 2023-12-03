12748ff358bd2c94b094521ac1c57cdd50ab73c6
Andres Arevalo Fajardo <a.arevalof@uniandes.edu.co>
2023-12-02 21:38:18 -0500

    get

--- balanceador_modulo_recursos/balanceador_modulo_recursos/views.py
+++ balanceador_modulo_recursos/balanceador_modulo_recursos/views.py
@@ -5,11 +5,18 @@ from django.views.decorators.csrf import csrf_exempt
 from .service import balanceador_modulo_recurso_service as service
 @csrf_exempt
 def send_request(request):
-    body = json.loads(request.body)
-    path = request.path
-    type = request.method
-    response = service.execute_requests_get(path,type)
-    return JsonResponse({'mensaje':'petición enviada'}, status=response.status_code)
+
+    if request.method == 'GET':
+        path = request.path
+        type = request.method
+        response = service.execute_requests_get(path,type)
+        return JsonResponse(response.json(), status=response.status_code)
+    else:
+        body = json.loads(request.body)
+        path = request.path
+        type = request.method
+        response = service.execute_requests(body,path,type)
+        return JsonResponse(response.json(), status=response.status_code)
 
 
 
